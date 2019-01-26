49e16d0fcd50b5a3efe5367c83e978a6a537294b
Ai Di Wang <aidiwang77@gmail.com>
2019-01-26 17:11:54 -0500

    added vertical scrolling

---
+++ yeehaw/Assets/Scripts/ScrollMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollMovement : MonoBehaviour
+
+{
+    private Rigidbody2D RIGIDBODY;
+    private float speed = 0.2f;
+    [SerializeField] bool stopScrolling;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RIGIDBODY = GetComponent<Rigidbody2D>();
+        RIGIDBODY.velocity = new Vector2(0, speed);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (stopScrolling)
+        {
+            RIGIDBODY.velocity = Vector2.zero;
+        }
+        else
+        {
+            RIGIDBODY.velocity = new Vector2(0, speed);
+        }
+        
+    }
+}
