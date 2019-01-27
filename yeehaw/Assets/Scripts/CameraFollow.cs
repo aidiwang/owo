60ac9c5306cdfd9497723fb48a4f2e14ae3190f8
Ai Di Wang <aidiwang77@gmail.com>
2019-01-27 12:45:04 -0500

    added player camera follow and sound effects

---
+++ yeehaw/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform player;
+    //public float speed = 0.125f;
+    public float cameraDistance = 30.0f;
+
+    private void Awake()
+    {
+        GetComponent<UnityEngine.Camera>().orthographicSize = ((Screen.height / 2) / cameraDistance);
+    }
+
+    private void FixedUpdate()
+    {
+        transform.position = new Vector3(transform.position.x, player.position.y,transform.position.z);
+        
+    }
+}
